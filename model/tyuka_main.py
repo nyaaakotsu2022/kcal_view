ab0486daddfa8572def0e854043f3a5294fc4b82
うい <86336067+My-me-admin@users.noreply.github.com>
2023-01-24 23:00:57 +0900

    Delete model directory

--- model/tyuka_main.py
+++
@@ -1,4 +0,0 @@
-from model.tyuka.tyuka import check_photo_tyuka
-from model.tyuka.tyuka2 import check_photo_tyuka2
-
-def tyuka_judge(foodpicture, number): return sorted([check_photo_tyuka(foodpicture, number), check_photo_tyuka2(foodpicture, number)], reverse=True)[0]
