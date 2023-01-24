9d30c836558c3f537a96b1aac82c774e50ffc347
うい <86336067+My-me-admin@users.noreply.github.com>
2023-01-24 23:24:48 +0900

    Delete model directory

--- model/wasyoku_main.py
+++
@@ -1,5 +0,0 @@
-from model.wasyoku.wasyoku import check_photo_wasyoku
-from model.wasyoku.wasyoku2 import check_photo_wasyoku2
-from model.wasyoku.wasyoku3 import check_photo_wasyoku3
-
-def wasyoku_judge(foodpicture, number): return sorted([check_photo_wasyoku(foodpicture, number), check_photo_wasyoku2(foodpicture, number), check_photo_wasyoku3(foodpicture, number)], reverse=True)[0]
